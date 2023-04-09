62294ea764b26661011e58a8f68b45179ec57fb0
ryan-don31 <rjdon9@gmail.com>
2023-04-09 03:25:30 -0400

    Updating files

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -1,35 +1,59 @@
 package com.example.webchatserver;
 
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import org.json.JSONObject;
 
 import java.io.IOException;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class represents a web socket server, a new connection is created and it receives a roomID as a parameter
  * **/
 @ServerEndpoint(value="/ws/{roomID}")
 public class ChatServer {
-
     // contains a static List of ChatRoom used to control the existing rooms and their users
 
+    private static Map<String,ChatRoom> roomlist = new HashMap<>();
+    //private static Map<>
     // you may add other attributes as you see fit
 
+    @GET
+    @Path('/roomlist')
+    @Produces('application/json')
+    public void getRooms(){
 
+    }
 
     @OnOpen
     public void open(@PathParam("roomID") String roomID, Session session) throws IOException, EncodeException {
-
-        session.getBasicRemote().sendText("First sample message to the client");
 //        accessing the roomID parameter
         System.out.println(roomID);
-
-
-
+        roomlist.put(roomID,new ChatRoom(roomID,session.getId()));
+        //roomList.put(session.getId(), roomID); // adding userID to a room
+//        // loading the history chat
+//        String history = loadChatRoomHistory(roomID);
+//        System.out.println("Room joined ");
+//        if (history!=null && !(history.isBlank())){
+//            System.out.println(history);
+//            history = history.replaceAll(System.lineSeparator(), "\\\n");
+//            System.out.println(history);
+//            session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\""+history+" \\n Chat room history loaded\"}");
+//            roomHistoryList.put(roomID, history+" \\n "+roomID + " room resumed.");
+//        }
+//        if(!roomHistoryList.containsKey(roomID)) { // only if this room has no history yet
+//            roomHistoryList.put(roomID, roomID + " room Created."); //initiating the room history
+//        }
+        session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server "+roomID+"): Welcome to the chat room. Please state your username to begin.\"}");
     }
 
     @OnClose
@@ -39,19 +63,36 @@ public class ChatServer {
     }
 
     @OnMessage
-    public void handleMessage(String comm, Session session) throws IOException, EncodeException {
-//        example getting unique userID that sent this message
-        String userId = session.getId();
+    public void handleMessage(String comm, Session session, @PathParam("roomID") String roomID) throws IOException, EncodeException {
+        //possible cases:
+        //first message = set message to their user name
+        //
 
-//        Example conversion of json messages from the client
-        //        JSONObject jsonmsg = new JSONObject(comm);
-//        String val1 = (String) jsonmsg.get("attribute1");
-//        String val2 = (String) jsonmsg.get("attribute2");
+        String userId = session.getId(); // userid of current user
+        JSONObject commjson = new JSONObject(comm); // json from message sent from javascript
+        String message = (String) commjson.get("msg"); // message being sent
+        ChatRoom currentRoom = roomlist.get(roomID); // current room as object
 
-        // handle the messages
+        Map<String,String> userList = currentRoom.getUsers(); // hashmap of all users in current room
 
+        if(userList.get(userId) != ""){// not their first message, the userid exists within the chatroom object with the userid
+            String username = userList.get(userId);
 
+            for(Session peer : session.getOpenSessions()){
+                if(currentRoom.inRoom(peer.getId())){
+                    peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(" + username + "): " + message + "\"}");
+                }
+            }
+        }else{
+            currentRoom.setUserName(session.getId(),message);
+            session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server "+roomID+"): Welcome, " + message + "!\"}");
+
+            for(Session peer: session.getOpenSessions()){
+                // only announce to those in the same room as me, excluding myself
+                if((!peer.getId().equals(userId))&&(currentRoom.inRoom(peer.getId()))){
+                    peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server): " + message + " joined the chat room.\"}");
+                }
+            }
+        }
     }
-
-
 }
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,4 +1,24 @@
 let ws;
+let roomcode;
+
+(function(){
+    document.getElementById("createbutton").addEventListener('click',newRoom)
+    document.getElementById("submit").addEventListener('click',send)
+    document.getElementsByClassName("inputbox")[0].addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            send()
+        }
+    });
+})()
+
+function send(){
+    var roomid = roomcode
+    var text = document.getElementsByClassName("inputbox")[0].value
+    let request = {"type":"chat", "msg":text}
+    console.log("sending message: "+request)
+    ws.send(JSON.stringify(request));
+    document.getElementsByClassName("inputbox")[0].value = "";
+}
 
 function newRoom(){
     // calling the ChatServlet to retrieve a new room ID
@@ -12,24 +32,41 @@ function newRoom(){
         .then(response => response.text())
         .then(response => enterRoom(response)); // enter the room with the code
 }
-function enterRoom(code){
 
+function enterRoom(code){
+    roomcode = code
+
+    addRoomList(code)
     // refresh the list of rooms
 
     // create the web socket
     ws = new WebSocket("ws://localhost:8080/WSChatServer-1.0-SNAPSHOT/ws/"+code);
 
-
     // parse messages received from the server and update the UI accordingly
     ws.onmessage = function (event) {
-        console.log(event.data);
+        console.log("Recieved message: "+event.data);
         // parsing the server's message as json
         let message = JSON.parse(event.data);
-
+        document.getElementById("textbox").value += "[" + timestamp() + "] " + message.message + "\n";
         // handle message
-
-        }
+    }
 }
 
+function timestamp() {
+    var d = new Date(), minutes = d.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    return d.getHours() + ':' + minutes;
+}
 
+function refreshRooms(){
+    //
+}
 
+function addRoomList(code){
+    var roomnum = String(code)
+    var el = document.createElement('p')
+    el.innerHTML = code
+    el.className = roomnum
+    var roomlist = document.getElementById('roomlist')
+    roomlist.appendChild(el)
+}
